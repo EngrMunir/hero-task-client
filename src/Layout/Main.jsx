300aeee17cbe99f9d31ef0b1d51e9919e5ea6c3d
Munir <smsirajulmonir@gmail.com>
2024-08-16 10:29:16 -0700

    navbar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../pages/Home/Navbar/Navbar';
 
 const Main = () => {
     return (
-        <div>
+        <div className='max-w-6xl mx-auto'>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
